8e45d10cb9486db612bfc6f4f7ae8e799b5a3b6f
Yuta Nakamura <nakamura1@cs.uchicago.edu>
2021-10-24 13:24:41 -0500

    done with H

--- working/H/MultiCast.java
+++ working/H/MultiCast.java
@@ -1,3 +1,44 @@
+import java.io.StringWriter;
+import java.io.StringReader;
+
+/* CDE: The encryption needed for signing the hash: */
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.Security;
+import java.security.Signature;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import javax.crypto.Cipher;
+import java.security.spec.*;
+// Ah, heck:
+import java.security.*;
+
+// Produces a 64-bye string representing 256 bits of the hash output. 4 bits per character
+import java.security.MessageDigest; // To produce the SHA-256 hash.
+
+/* CDE Some other uitilities: */
+
+import java.util.Date;
+import java.util.Random;
+import java.util.UUID;
+import java.text.*;
+import java.util.Base64;
+import java.util.Arrays;
+
+/*
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+*/
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.Reader;
+
 /*
  * Thanks: http://www.javacodex.com/Concurrency/PriorityBlockingQueue-Example
  */
@@ -9,46 +50,48 @@ import java.util.concurrent.*;
 
 class Ports 
 {
-    public static int PortBase = 4710;
+    public static int publicKeyPortBase = 4710;
     public static int Port;
 
     public void setPorts()
     {
-        Port = PortBase + MultiCast.pnum; 
+        Port = publicKeyPortBase + MultiCast.pnum; 
     }
 }
 
-class Worker extends Thread 
+class publicKeyReceivingWorker extends Thread 
 {
     Socket sock;
-    Worker (Socket s) {sock = s;}
+    publicKeyReceivingWorker (Socket s) {sock = s;}
 
     public void run() 
     {
         try 
         {
             BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
-            String data = in.readLine();
-            System.out.println(data);
+            String pnum = in.readLine();
+            String StringKey = in.readLine();
             sock.close();
+            System.out.println("from Process " + pnum + ":\n" + StringKey);
         } catch (IOException x) {x.printStackTrace();}
 
     }
 }
 
-class server implements Runnable 
+class publicKeyReceivingServer implements Runnable 
 {
     public void run() 
     {
         int q_len = 6;
         Socket sock;
+        System.out.println("Public Key Receiving Server has started");
         System.out.println("ready to listen at Port " + Ports.Port);
         try {
             ServerSocket servsock = new ServerSocket(Ports.Port, q_len);
             while(true)
             {
                 sock = servsock.accept();
-                new Worker (sock).start();
+                new publicKeyReceivingWorker (sock).start();
             }
         } catch (IOException ioe) {System.out.println(ioe);}
     }
@@ -66,7 +109,9 @@ public class MultiCast
     public static void main(String argv[]) 
     {
         MultiCast m = new MultiCast(argv);
-        m.run(argv);
+        try {
+            m.run(argv);
+        } catch (Exception x) {}
     }
 
     public MultiCast(String argv[])
@@ -75,14 +120,17 @@ public class MultiCast
         pnum = 0;
     }
 
-    public void run(String argv[])
+    public void run(String argv[]) throws Exception
     {
         System.out.println("Running now\n");
-        try {
-            DemonstrateUtilities(argv);
-        } catch (Exception x) {};
-        new Thread(new server()).start();
-        sendMessage();
+        DemonstrateUtilities(argv);
+
+        KeyPair keyPair = generateKeyPair(999);
+        PrivateKey privateKey = keyPair.getPrivate();
+        PublicKey publicKey = keyPair.getPublic();    
+ 
+        new Thread(new publicKeyReceivingServer()).start();
+        sendPublicKey(publicKey);
     }
 
     public void DemonstrateUtilities(String args []) throws Exception
@@ -110,28 +158,41 @@ public class MultiCast
         System.out.println("Hello from Process " + pnum + "\nPort: " + Ports.Port + "\n");
     }
 
-    public void sendMessage()
+    public void sendPublicKey(PublicKey publicKey)
     {
         Socket sock;
         PrintStream toServer;
-        while(true)
-        {
-            try{Thread.sleep(3000);} catch (Exception x) {}
 
-            for(int i = 0; i < numProcesses; i++)
-            {
-                System.out.println("From Process " + pnum + " to " + i);
-                try {
-                    sock = new Socket(serverName, Ports.PortBase + i); 
-                    toServer = new PrintStream(sock.getOutputStream());
-                    toServer.println("Hello multicast message from Process " + pnum);
-                    toServer.flush();
-                    sock.close();
-                } catch (Exception x) {
-                    System.out.println("did not work when trying to send to Process " + i);
-                }
+        byte[] bytePubKey = publicKey.getEncoded();
+        String stringKey = Base64.getEncoder().encodeToString(bytePubKey);
+
+        try{Thread.sleep(10000);} catch (Exception x) {}
+
+        for(int i = 0; i < numProcesses; i++)
+        {
+            System.out.println("From Process " + pnum + " to " + i);
+            try {
+                sock = new Socket(serverName, Ports.publicKeyPortBase + i); 
+                toServer = new PrintStream(sock.getOutputStream());
+                //toServer.println("Hello multicast message from Process " + pnum);
+                toServer.println(pnum);
+                toServer.flush();
+                toServer.println(stringKey);
+                toServer.flush();
+                sock.close();
+            } catch (Exception x) {
+                System.out.println("did not work when trying to send to Process " + i);
             }
         }
     }
-    
+
+    public static KeyPair generateKeyPair(long seed) throws Exception 
+    {
+        KeyPairGenerator keyGenerator = KeyPairGenerator.getInstance("RSA");
+        SecureRandom rng = SecureRandom.getInstance("SHA1PRNG", "SUN");
+        rng.setSeed(seed);
+        keyGenerator.initialize(1024, rng);
+        return (keyGenerator.generateKeyPair());
+    }
+   
 }
