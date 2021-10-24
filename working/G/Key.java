f1ef117e3a755d391bcf78688d76db926f0b146f
Yuta Nakamura <yutanak6@gmail.com>
2021-10-24 12:19:19 -0500

    deleted some lines

--- working/G/Key.java
+++ working/G/Key.java
@@ -20,7 +20,6 @@ import java.security.*;
 // Produces a 64-bye string representing 256 bits of the hash output. 4 bits per character
 import java.security.MessageDigest; // To produce the SHA-256 hash.
 
-
 /* CDE Some other uitilities: */
 
 import java.util.Date;
@@ -38,45 +37,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.FileReader;
 import java.io.Reader;
-
-/*
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.PrivateKey;
-import java.security.PublicKey;
-import java.security.SecureRandom;
-import java.security.Security;
-import java.security.Signature;
-import java.security.NoSuchAlgorithmException;
-
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.PKCS8EncodedKeySpec;
-import java.security.spec.*;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.PrivateKey;
-import java.security.PublicKey;
-import java.security.SecureRandom;
-import java.security.Security;
-import java.security.Signature;
-import java.security.NoSuchAlgorithmException;
-import java.security.spec.PKCS8EncodedKeySpec;
-import javax.crypto.Cipher;
-import java.security.spec.*;
-// Ah, heck:
-import java.security.*;
-
-// Produces a 64-bye string representing 256 bits of the hash output. 4 bits per character
-import java.security.MessageDigest;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.FileReader;
-import java.io.Reader;
-*/
 
 class Key 
 {
@@ -105,15 +65,20 @@ class Key
         publicKey = keyPair.getPublic();    
 
         WriteJSON(publicKey);
-        ReadJSON();
+        String stringKey = ReadJSON();
+        byte[] bytePubkey = Base64.getDecoder().decode(stringKey);
+        X509EncodedKeySpec pubSpec = new X509EncodedKeySpec(bytePubkey);
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        PublicKey RestoredKey = keyFactory.generatePublic(pubSpec);
+        System.out.println("public key: " + publicKey);
+
+        
     }
     
     public void WriteJSON(PublicKey publicKey)
     {
-        System.out.println("public key: " + publicKey);
         byte[] bytePubKey = publicKey.getEncoded();
         String stringKey = Base64.getEncoder().encodeToString(bytePubKey);
-        System.out.println("Key in String form: " + stringKey);
 
         Gson gson = new GsonBuilder().create();
         try(FileWriter writer = new FileWriter("record.json"))
@@ -125,8 +90,19 @@ class Key
         }
     }
 
-    public void ReadJSON()
+    public String ReadJSON()
     {
+        Gson gson = new Gson();
+        String rv = "";
+        try (Reader reader = new FileReader("record.json"))
+        {
+            rv = gson.fromJson(reader, String.class);
+        } 
+        catch (IOException e) 
+        {
+            e.printStackTrace();
+        }
+        return rv;
     }
     
     public static KeyPair generateKeyPair(long seed) throws Exception 
