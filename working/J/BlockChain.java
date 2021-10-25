4a85362aedbe2eddb6b1bd98725b5392fb5d9379
Yuta Nakamura <yutanak6@gmail.com>
2021-10-25 15:08:21 -0500

    at J

--- working/J/BlockChain.java
+++ working/J/BlockChain.java
@@ -17,8 +17,134 @@ import java.security.MessageDigest; // To produce the SHA-256 hash.
 
 import java.util.Date;
 import java.util.UUID;
-import java.util.Scanner;
+//import java.util.Scanner;
 import java.util.Arrays;
+import java.util.Random;
+import java.text.*;
+import java.util.Base64;
+
+import java.io.StringWriter;
+import java.io.StringReader;
+
+/*
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+*/
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.Reader;
+
+/*
+ * Thanks: http://www.javacodex.com/Concurrency/PriorityBlockingQueue-Example
+ */
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.util.concurrent.*;
+
+class Ports 
+{
+    public static int publicKeyPortBase = 4710;
+    public static int blockChainPortBase = 4930;
+    public static int publicKeyPort;
+    public static int blockChainPort;
+
+    public void setPorts()
+    {
+        publicKeyPort = publicKeyPortBase + BlockChain.pnum; 
+        blockChainPort = blockChainPortBase + BlockChain.pnum;
+    }
+}
+
+class publicKeyReceivingWorker extends Thread 
+{
+    Socket sock;
+    publicKeyReceivingWorker (Socket s) {sock = s;}
+
+    public void run() 
+    {
+        try 
+        {
+            BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+            String pnum = in.readLine();
+            String StringKey = in.readLine();
+            sock.close();
+            //System.out.println("from Process " + pnum + ":\n" + StringKey);
+        } catch (IOException x) {x.printStackTrace();}
+
+    }
+}
+
+class publicKeyReceivingServer implements Runnable 
+{
+    public void run() 
+    {
+        int q_len = 6;
+        Socket sock;
+        System.out.println("Public Key Receiving Server has started");
+        System.out.println("ready to listen at Port " + Ports.Port);
+        try {
+            ServerSocket servsock = new ServerSocket(Ports.Port, q_len);
+            while(true)
+            {
+                sock = servsock.accept();
+                new publicKeyReceivingWorker (sock).start();
+            }
+        } catch (IOException ioe) {System.out.println(ioe);}
+    }
+}
+
+class publicKeySender implements Runnable
+{
+
+    public void run()
+    {
+        
+    }
+
+    public void sendPublicKey(PublicKey publicKey)
+    {
+        Socket sock;
+        PrintStream toServer;
+
+        byte[] bytePubKey = publicKey.getEncoded();
+        String stringKey = Base64.getEncoder().encodeToString(bytePubKey);
+
+        try{Thread.sleep(10000);} catch (Exception x) {}
+
+        for(int i = 0; i < numProcesses; i++)
+        {
+            //System.out.println("From Process " + pnum + " to " + i);
+            try {
+                sock = new Socket(serverName, Ports.publicKeyPortBase + i); 
+                toServer = new PrintStream(sock.getOutputStream());
+                //toServer.println("Hello multicast message from Process " + pnum);
+                toServer.println(pnum);
+                toServer.flush();
+                toServer.println(stringKey);
+                toServer.flush();
+                sock.close();
+            } catch (Exception x) {
+                System.out.println("did not work when trying to send to Process " + i);
+            }
+        }
+    }
+
+    public static KeyPair generateKeyPair(long seed) throws Exception 
+    {
+        KeyPairGenerator keyGenerator = KeyPairGenerator.getInstance("RSA");
+        SecureRandom rng = SecureRandom.getInstance("SHA1PRNG", "SUN");
+        rng.setSeed(seed);
+        keyGenerator.initialize(1024, rng);
+        return (keyGenerator.generateKeyPair());
+    }
+   
+
+}
+
 
 class BlockRecord{
   //String BlockID;
@@ -178,6 +304,9 @@ class Block {
 
 public class BlockChain 
 {
+    static String serverName = "localhost";
+    static int numProcesses = 3;
+    public static int pnum;
     int count; 
     Block tail;
     Block head;
@@ -185,6 +314,21 @@ public class BlockChain
     public static void main(String argv[])
     {
         //System.out.println("hello world");
+        /*
+        int UnverifiedBlockPort;
+        int BlockChainPort;
+        */
+
+        if (argv.length > 2) System.out.println("Special functionality present\n");
+
+        if (argv.length < 1) pnum = 0;
+        else if (argv[0].equals("0")) pnum = 0;
+        else if (argv[0].equals("1")) pnum = 1;
+        else if (argv[0].equals("2")) pnum = 2;
+        else pnum = 0;
+
+        new Ports().setPorts();
+
         BlockChain bc = new BlockChain(argv);
         bc.run(argv);
     }
@@ -208,20 +352,11 @@ public class BlockChain
         catch (Exception x){};
     }
 
-    public void DemonstrateUtilities(String args[]) throws Exception 
+    public void DemonstrateUtilities(String argv[]) throws Exception 
     {
-        /*
-         * 1. create a genesis block and make a random string
-         * 2. hash those values with no prevHashValue
-         * 3. add that as a tail and a head 
-         * 4. create other 3 blocks and concatenate them together 
-         */
-
         String prevHash = "";
-        // 1. create a genesis block
         Block genesis = new Block(prevHash);
         prevHash = genesis.createHV();
-        //System.out.println(prevHash);
         appendGenesis(genesis);
 
         Block newBlock;
